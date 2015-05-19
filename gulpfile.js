cfea5e5ad32e0ffddaa2807f57a4e8224ac794b1
xtornasol512 <xtornasol512@gmail.com>
2015-05-19 00:10:06 -0500

    agregando gulpfile and package json  con dependencia s de node

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,47 @@
 var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    plumber = require('gulp-plumber')
     rename = require('gulp-rename'),
     prefix = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'), 
-    //concat = require('gulp-concat'),
     uglify = require('gulp-uglify');
 
-
-var srcCSS = 'front-end/css/*.css',
-    distCSS = 'front-end/css/min/',
-    distPrefix = 'front-end/css/prefixed',
-    srcJS = 'front-end/js/site/*.js', 
+//Direcciones de Archivos: CSS's y JS's
+var srcCSS = 'front-end/css/src/*.css',
+    //distPrefix es el CSS con los prefijos de los navegadores
+    distPrefix = 'front-end/css/prefix',
+    distCSS = 'front-end/css/',
+    srcJS = 'front-end/js/src/*.js', 
     distJS = 'front-end/js/';
 
+//Task para los estilos CSS
 gulp.task('styl', function(){
     gulp.src(srcCSS)
-        .pipe(prefix(['safari 5', 'ff 17', 'ie 10', 'opera 12.1', 'ios 5', 'android 2.2'])) //
+        .pipe(plumber())
+        .pipe(prefix(['safari 5', 'ff 24', 'ie 10', 'opera 12.1', 'ios 5', 'android 4'])) //
         .pipe(gulp.dest(distPrefix))
         .pipe(minifycss())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest(distCSS));
 });
 
+//Task para archivos Javascript
 gulp.task('scripts', function(){
     gulp.src(srcJS)
+        .pipe(plumber())
         .pipe(uglify())
-        //.pipe(concat('all.js'))
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest(distJS));
 });
 
-/*gulp.task('watch', function(){
+//Task para supervisar cambios
+gulp.task('watch', function(){
     gulp.watch(srcCSS, ['styl']);
     gulp.watch(srcJS, ['scripts']);
-    gulp.watch(srcJS, ['scripts']);
-    gulp.watch(srcHtml, ['htmls'])
-});*/
-gulp.task('default', function(){
-    console.log('Welcome from PhyroServer!');
-    gulp.watch(srcCSS, ['styl']);
-    gulp.watch(srcJS, ['scripts']);
+});
+
+//Task default
+//Se ejecuta con $ gulp
+gulp.task('default',['styl', 'scripts', 'watch'], function(){
+    console.log('Welcome to Ontomex FrontEnd!');
 });
