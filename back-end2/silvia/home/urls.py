fd142b1f86a45719d9a0a1f719f1f872fb3f6a35
vash512 <phyrox.vash512@gmail.com>
2015-05-19 13:58:26 -0500

    urls

---
+++ back-end2/silvia/home/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns=patterns('home.views',
+    url(r'^$','index'),
+    url(r'^home/$','home'),
+    url(r'^venta/$','venta'),
+    url(r'^logout/$','log_out'),
+    #url(r'^tag/(?P<tag>[\w\-]+)/$','tag_aviso'),
+    #url(r'^(?P<perfil>[\w\-]+)/$','perfil_view'),
+)
